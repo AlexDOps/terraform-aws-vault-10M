4998d60a0b95c7d93866e156e08bcb9ce7e3c6cd
AlexDOps <92064331+AlexDOps@users.noreply.github.com>
2023-05-24 18:58:17 -0400

    Update variables.tf

--- examples/mysubnet/variables.tf
+++ examples/mysubnet/variables.tf
@@ -1,4 +1,4 @@
 variable "domain" {
-  default     = "chawlersolutions.net"
+  default     = "vault.chawlersolutions.net"
   description = "The domain to use for the deployment. This domain should be hosted on AWS."
 }
